f06896edd50d0632c820df5c8c785065a7b7748f
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2022-11-28 19:47:29 +0900

    Feat: navigation bar 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-import type { NextPage } from 'next';
-import { getSession, signOut, useSession } from 'next-auth/react';
-import Head from 'next/head';
+import type { GetServerSideProps, NextPage } from 'next';
+import { getSession, useSession } from 'next-auth/react';
+
 import Link from 'next/link';
-import axios from 'axios';
 
 import styles from '../styles/Home.module.css';
+import Layout from '@components/layout/Layout';
+import Seo from '@components/layout/Seo';
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
@@ -12,13 +13,9 @@ const Home: NextPage = () => {
 
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Me-Moonjang</title>
-        <meta name="description" content="me-moonjang" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Seo title="Home" />
 
-      {session ? mainPage({ session }) : gate()}
+      {session ? mainPage() : gate()}
     </div>
   );
 };
@@ -37,45 +34,17 @@ const gate = () => {
   );
 };
 
-const mainPage = ({ session }: any) => {
-  const { id } = session.user;
-
-  const handleLogout = async () => {
-    try {
-      const res = await axios.post(`api/auth/logout`, {
-        id,
-      });
-
-      if (res.status === 200) {
-        signOut();
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
+const mainPage = () => {
   return (
-    <main className="container mx-auto text-center mt-20">
-      <h3 className="text-4xl font-bold">Me-Moonjang</h3>
-
-      <div className="details">
-        <h5>{session.user.name}</h5>
-        <h5>{session.user.email}</h5>
-      </div>
-
-      <div className="flex justify-center">
-        <button
-          onClick={handleLogout}
-          className="mt-5 px-10 py-1 rounded-sm bg-indigo-500"
-        >
-          로그아웃
-        </button>
-      </div>
-    </main>
+    <Layout>
+      <main className="container mx-auto text-center mt-20 p-5">
+        <h3 className="text-4xl font-bold">Me-Moonjang</h3>
+      </main>
+    </Layout>
   );
 };
 
-export async function getServerSideProps(context: any) {
+export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context);
 
   return {
@@ -83,6 +52,6 @@ export async function getServerSideProps(context: any) {
       session,
     },
   };
-}
+};
 
 export default Home;
