9ebce52ae2b908cee0a05e6e231527d625eb550c
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2022-12-01 11:47:37 +0900

    Feat: profile page 및 회원탈퇴 로직 구현

---
+++ pages/api/auth/deleteuser.ts
@@ -0,0 +1,27 @@
+import dbConnect from '@lib/db';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const deleteUser = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { email } = req.body;
+
+  try {
+    const client = await dbConnect();
+    const db = client.db();
+    const usersCollection = db.collection('users');
+    const response = await usersCollection.deleteOne({
+      email,
+    });
+    console.log(response, 'user delete console');
+
+    if (response.deletedCount === 0) {
+      client.close();
+      throw new Error('No user deleted');
+    }
+    res.status(200).json({ message: 'User deleted' });
+    client.close();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default deleteUser;
