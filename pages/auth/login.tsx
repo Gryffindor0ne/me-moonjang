5c39ffc63f91386794b58863c9e8d3f5b5fa19e8
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2023-01-02 16:55:32 +0900

    Fix: 로그인, 로그아웃 수정

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import type { NextPage } from 'next';
 import Link from 'next/link';
-import { signIn, getSession } from 'next-auth/react';
+import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import * as yup from 'yup';
