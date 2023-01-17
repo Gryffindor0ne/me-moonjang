be9802b3a15fce93a444c7ae641d42944f02ed5c
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2023-01-17 18:05:32 +0900

    Refactor: splash 화면 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import type { GetServerSideProps } from 'next';
 import { getSession, useSession } from 'next-auth/react';
 import {
@@ -7,9 +7,11 @@ import {
   useQuery,
   useQueryClient,
 } from '@tanstack/react-query';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 import axios from 'axios';
+import { useTrail, a } from '@react-spring/web';
 
+import styles from '@styles/styles.module.css';
 import Layout from '@components/layout/Layout';
 import Seo from '@components/layout/Seo';
 import GroupBoard from '@components/groups/GroupBoard';
@@ -29,10 +31,35 @@ export const getGroupsData = async (
   return data;
 };
 
+const Trail: React.FC<{ open: boolean; children: any }> = ({
+  open,
+  children,
+}) => {
+  const items = React.Children.toArray(children);
+  const trail = useTrail(items.length, {
+    config: { mass: 5, tension: 2000, friction: 200 },
+    opacity: open ? 1 : 0,
+    x: open ? 0 : 20,
+    height: open ? 150 : 0,
+    from: { opacity: 0, x: 30, height: 0 },
+  });
+  return (
+    <div>
+      {trail.map(({ height, ...style }, index) => (
+        <a.div key={index} className={styles.trailsText} style={style}>
+          <a.div style={{ height }}>{items[index]}</a.div>
+        </a.div>
+      ))}
+    </div>
+  );
+};
+
 const Home = () => {
   const { data: session } = useSession();
   const user = session?.user as UserInfo;
   const queryClient = useQueryClient();
+
+  const [open, setIsOpen] = useState(true);
 
   const {
     data: groups,
@@ -40,6 +67,13 @@ const Home = () => {
     isError,
     error,
   } = useQuery(['groupsData', user], () => getGroupsData(user));
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+  }, []);
 
   useEffect(() => {
     if (groups) {
@@ -64,23 +98,37 @@ const Home = () => {
     <div>
       <Seo title="Home" />
 
-      {session ? mainPage({ groups }) : gate()}
+      {session ? Main({ groups }) : Splash({ open, setIsOpen })}
     </div>
   );
 };
 
-const gate = () => {
+const Splash = ({
+  open,
+  setIsOpen,
+}: {
+  open: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}) => {
+  const router = useRouter();
+
+  setTimeout(() => router.push('/auth/login'), 3000);
+
   return (
-    <Link
-      href="/auth/login"
-      className="flex items-center justify-center w-full h-screen"
+    <div
+      className="flex items-center justify-center w-full h-screen bg-teal-500"
+      onClick={() => setIsOpen((prev) => !prev)}
     >
-      <span className="text-4xl font-bold text-gray-600">Me Moonjang</span>
-    </Link>
+      <Trail open={open}>
+        <span className="text-3xl font-bold text-white md:text-4xl">
+          Me Moonjang
+        </span>
+      </Trail>
+    </div>
   );
 };
 
-const mainPage = ({ groups }: { groups: string[] | undefined }) => {
+const Main = ({ groups }: { groups: string[] | undefined }) => {
   return (
     <Layout>
       <GroupBoard groups={groups} />
