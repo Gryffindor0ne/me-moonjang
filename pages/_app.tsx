60684ee44863c29f93a50e1a73a752688a69da4d
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2023-03-01 21:53:54 +0900

    Refactor: Recoil 셋업

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { Hydrate, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ChakraProvider } from '@chakra-ui/react';
 import { createStandaloneToast } from '@chakra-ui/toast';
+import { RecoilRoot } from 'recoil';
 
 import '../styles/globals.css';
 import { theme } from 'theme';
@@ -16,16 +17,18 @@ const { ToastContainer } = createStandaloneToast();
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <ChakraProvider theme={theme}>
-        <QueryClientProvider client={queryClient}>
-          <Hydrate state={pageProps.dehydratedState}>
-            <Component {...pageProps} />
-            <Loading />
-            <ToastContainer />
-          </Hydrate>
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-      </ChakraProvider>
+      <RecoilRoot>
+        <ChakraProvider theme={theme}>
+          <QueryClientProvider client={queryClient}>
+            <Hydrate state={pageProps.dehydratedState}>
+              <Component {...pageProps} />
+              <Loading />
+              <ToastContainer />
+            </Hydrate>
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
+        </ChakraProvider>
+      </RecoilRoot>
     </SessionProvider>
   );
 }
